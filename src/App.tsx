563f6be20810bcbcf02b9bcf531026fcbd434fb4
fabriciopashaj <fabriciopashaj.dev@gmail.com>
2023-08-10 12:29:47 +0300

    feat: Implemented first plain version of the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
+type TodoData = {
+  content: string,
+  completed: boolean
+};
+
+type TodoCallbacks = {
+  toggleChecked: (id: number) => void,
+  remove: (id: number) => void
+};
+
+type TodoProps = {
+  id: number,
+  data: TodoData,
+  callbacks: TodoCallbacks,
+};
+
+function Todo({ id, data, callbacks }: TodoProps) {
+  const { content, completed } = data;
+  const { toggleChecked, remove } = callbacks;
+  const [checked, setChecked] = useState(completed);
+  const handleChange = () => {
+    setChecked(!checked);
+    toggleChecked(id);
+  }
+  return (
+    <div className="todo-item">
+      <span>{checked ? <s>{content}</s> : content}</span>
+      <input type="checkbox" checked={checked} onChange={handleChange} />
+      <button onClick={() => remove(id)}>X</button>
+    </div>
+  );
+}
+
 function App() {
+  const [state, setState] = useState({
+    todos: [{ content: 'Creat todo app', completed: false } as TodoData]
+  });
+  //                      like man wtf but whatever
+  const inputRef = useRef(null as unknown as HTMLInputElement);
+  const addTodo = () => {
+    const content = inputRef.current.value.trim();
+    if (content === '') return;
+    state.todos.push({ content, completed: false });
+    setState({ todos: state.todos });
+    inputRef.current.value = '';
+  };
+  const callbacks = {
+    toggleChecked(id: number) {
+      state.todos[id].completed = !state.todos[id].completed;
+    },
+    remove(id: number) {
+      state.todos.splice(id, 1);
+      setState({ todos: state.todos });
+    },
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {state.todos.map((todoData, i) => {
+          return (
+            <Todo
+              key={i}
+              id={i}
+              data={todoData}
+              callbacks={callbacks}
+            />
+          );
+        })}
+      </div>
+      <input type="text" ref={inputRef} />
+      <button onClick={addTodo}>Add</button>
     </div>
   );
 }
